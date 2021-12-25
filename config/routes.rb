7adf88708a5da6e0f2a7339307a60aa807507241
emerson espinoza <xinayespinoza@gmail.com>
2021-12-25 19:36:26 -0300

    Agregamos la ruta de inicio

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   resources :posts do
     resources :comments
   end
+
+  root to: "posts#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
